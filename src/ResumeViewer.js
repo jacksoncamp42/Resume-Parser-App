1fd64775883c88ad3d58edcae4f0958f19e515ea
Robert Balatbat <73982521+robjgb@users.noreply.github.com>
2022-08-11 15:07:45 -0700

    Resume View Updates

--- src/ResumeViewer.js
+++ src/ResumeViewer.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import ResumeContainer from './components/ResumeContainer';
 
 export default function ResumeViewer() {
-  return <div>ResumeViewer</div>;
+  return <>
+  <div>ResumeViewer</div>
+  <ResumeContainer/>
+  </>
 }
