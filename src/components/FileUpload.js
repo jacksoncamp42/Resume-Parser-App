aa4d215cd87e5516f0f0e0b0b697ddce0f5be163
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-08-11 19:46:21 -0400

    upload page components

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,48 +1,53 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
+import UploadPic from './sidebar-icons/UploadPic.png';
 
 const FileUpload = () => {
-  const [file, setFile] = useState("");
-  const [filename, setFilename] = useState("Choose File");
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
   const [uploadedFile, setUploadedFile] = useState({});
   const [items, setItems] = useState([]);
   const [submitted, setSubmitted] = useState(false);
-  const [searchCategory, setSearchCategory] = useState("first_name");
-  const [filter, setFilter] = useState("");
+  const [searchCategory, setSearchCategory] = useState('first_name');
+  const [filter, setFilter] = useState('');
 
-  const onChange = (e) => {
-    
-  };
+  const onChange = (e) => {};
 
-
-  const onSubmit = async (e) => {
-    
-  };
+  const onSubmit = async (e) => {};
 
   const changeFilter = (e) => {
     setFilter(e.target.value);
   };
 
   return (
-    <Fragment>
-      <form onSubmit={onSubmit}>
-        <div className="custom-file mb-4">
-          <input
-            type="file"
-            className="custom-file-input"
-            id="customFile"
-            onChange={onChange}
-          />
-          <label className="custom-file-label" htmlFor="customFile">
-            {filename}
-          </label>
+    <div className="upload-container">
+      <div className="left-side">
+        <h1>Welcome, User!</h1>
+        <div className="left-shape">
+          <h2>Upload Your Resume</h2>
+          <img src={UploadPic} alt="upload-logo" />
         </div>
-        <input
-          type="submit"
-          value="Upload"
-          className="btn btn-primary btn-block mt-4"
-        />
-      </form>
-    </Fragment>
+      </div>
+      <Fragment>
+        <form onSubmit={onSubmit}>
+          <div className="custom-file mb-4">
+            <input
+              type="file"
+              className="custom-file-input"
+              id="customFile"
+              onChange={onChange}
+            />
+            <label className="custom-file-label" htmlFor="customFile">
+              {filename}
+            </label>
+          </div>
+          <input
+            type="submit"
+            value="Submit Resume"
+            className="btn btn-primary btn-block mt-4"
+          />
+        </form>
+      </Fragment>
+    </div>
   );
 };
 
