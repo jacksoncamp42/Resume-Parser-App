732c71ac2b9c1cd2025c16b3c4daf8704639692f
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-08-11 18:44:20 -0400

    added sidebar and search

--- src/ResumeViewer.js
+++ src/ResumeViewer.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Sidebar } from './components/Sidebar';
+import Search from './components/Search';
+import './ResumeViewer.css';
 
 export default function ResumeViewer() {
-  return <div>ResumeViewer</div>;
+  return (
+    <div className="main-container">
+      <Search />
+    </div>
+  );
 }
