ea99b196bcd4212f370f177e09b54b3991394ad2
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-12 13:28:34 -0700

    Update Amplify CLI settings to match Jackson's.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "resumeparsere8c1a466": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "ResumeStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
